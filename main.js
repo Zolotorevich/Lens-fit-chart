0b1094eb2893124aceb43e5a2a3f8c73a7684fc7
Roman Zolotorevich <roman@zolotorevich.com>
2021-12-22 20:56:36 +0300

    js minor changes

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
 //Function calculates diameter for lens circle
 function diameterCalculation(width,height) {
+	console.log('diameterCalculation(' + width + ', ' + height + ')');
+
 	//calculate hypotenuse
 	hypotenuse = Math.sqrt((width ** 2) + (height ** 2));
+	console.log('hypotenuse = ' + hypotenuse);
 
 	//add margin
 	hypotenuse += hypotenuse / 10;
+	console.log('hypotenuse + 10% = ' + hypotenuse);
 
 	//return round result
+	console.log('diameterCalculation result = ' + Math.round(hypotenuse));
 	return (Math.round(hypotenuse));
-
 }
 
+function drawSensor(type,width,height) {
+	$("#sensor_square").width(width);
+	$("#sensor_square").height(height);
+}
+
+function drawLens(type,diameter) {
+	$("#lens_circle").width(diameter);
+	$("#lens_circle").height(diameter);
+}
+
+function hypotenuse(a,b) {
+	return Math.sqrt((a ** 2) + (b ** 2));
+}
